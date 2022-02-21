0607f1c0b1cac8812f49f70f0cd69c3d1af92b51
syedsohail191 <sdsohail191@gmail.com>
2022-02-21 18:31:28 +0530

    Sd-component updated

--- github-practice/src/App.js
+++ github-practice/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Testing from "./Components/KHWAJA/Testing";
+import Sohail from "./Components/Sohail/Sohail";
 function App() {
   return (
     <div className="App">
       <Testing />
+      <Sohail />
     </div>
   );
 }
---
+++ github-practice/src/Components/Sohail/Sohail.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const Sohail = () => {
+    return (
+        <div>
+            <h1>Sohail</h1>
+        </div>
+    );
+};
+
+export default Sohail;
