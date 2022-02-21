56357b63ec2400ea96d6e4a68d6efbc3a95c59c7
Abhipawde4287 <97161900+Abhipawde4287@users.noreply.github.com>
2022-02-21 16:54:10 +0530

    xyz

--- github-practice/src/App.js
+++ github-practice/src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import Abhi from "./Components/KHWAJA/Abhi/Abhi";
 import Testing from "./Components/KHWAJA/Testing";
 function App() {
   return (
     <div className="App">
+      <Abhi/>
       <Testing />
     </div>
   );
---
+++ github-practice/src/Components/KHWAJA/Abhi/Abhi.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Abhi = () => {
+    return (
+        <div>
+            <h1>Hello Abhi</h1>
+        </div>
+    );
+};
+
+export default Abhi;
