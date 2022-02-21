329deb749f5496786c5002b7f72dc1d82d4afc88
ShaikhJavid <shaikhjavid06@gmail.com>
2022-02-21 19:02:07 +0530

    SJ PDac

--- github-practice/src/App.js
+++ github-practice/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
-import Testing from "./Components/KHWAJA/Testing";
+// import Testing from "./Components/KHWAJA/Testing";
+import Testing from "./Components/SHAIKHJAVID/Testing";
 function App() {
   return (
     <div className="App">
-      <Testing />
+      {/* <Testing /> */}
+      <Testing/>
     </div>
   );
 }
