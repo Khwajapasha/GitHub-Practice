12ecaf7c0c1bb5a6251048214d5f9ef60bc4cee3
mukassar <shaikhmukassar@gmail.com>
2022-02-22 15:34:32 +0530

    SK- added component

--- github-practice/src/App.js
+++ github-practice/src/App.js
@@ -5,15 +5,18 @@ import Sameer from "./Components/Sameer/Sameer";
 import Mukassar from "./Components/MUKASSAR/Mukassar";
 import Amena from "./Components/AMENA/Amena";
 import AfrozSir from "./Components/AFROZ_SIR/AfrozSir";
+import ResolveConflict from "./Components/ResolveConflict/ResolveConflict";
 function App() {
   return (
     <div className="App">
       <Testing />
+      <ResolveConflict/>
       <Sameer/>
       <Mukassar/>
       <IrfanShah />
       <Amena />
       <AfrozSir />
+      
 
     </div>
   );
